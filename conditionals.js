118443c7237af8f6688a84556399ffb47c3deb19
Denny <Dennyng1989@hotmail.com>
2022-12-30 19:29:49 -0800

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,11 +1,12 @@
-const raining = true;
-const temperature = -1;
+// first example, using only raining and temperature as base conditionals
+const raining = false; //define if it is raining or not
+const temperature = -41; //define exterior temperature
 
-if (raining) {
+if (raining) { //check if it is raining
   console.log("Don't forget your umbrella!");
 }
 
-if (temperature < 0) {
+if (temperature < 0) { //determine log output based on temperature
   console.log("Make sure you pick out a scarf!");
 } else if (temperature < 15) {
   console.log("Short sleeves won't cut it!");
@@ -14,3 +15,22 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+//second example, using logical operators for voting
+const isCitizen = true;
+const age = 26;
+if (isCitizen === true && age > 18) {
+  console.log("You are eligible to vote.")
+}
+
+
+//futher conditionals using logical operators for weather case
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea...")
+}
+
+//further conditionals to use !raining 
+
+if (!raining) {
+  console.log("Leave your umbrella at home!")
+}
